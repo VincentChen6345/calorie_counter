bea140dd1b4aa8049c726107efe56ba3af4dab78
jimei chen <jimeichen@jimeideiMac.local>
2021-11-05 15:20:43 +1100

    boxshadow

--- lib/components/variables.dart
+++ lib/components/variables.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-const primary_Color= Color(0xffFFBC0E);
+const primary_Color = Color(0xffFFBC0E);
 
-const backgroundColor=Color(0xFFDB0006);
+const backgroundColor = Color(0xFFDB0006);
 
-const activeCardColor= primary_Color;
-const inactiveCardColor=Color(0xFFFFD982);
+const activeCardColor = primary_Color;
+const inactiveCardColor = Color(0xFFFFD982);
 
-List<BoxShadow> boxShadow=[BoxShadow(
-    color: Colors.black.withOpacity(0.4),
-    spreadRadius: 2,
-    blurRadius: 2,
-    offset: Offset(3,3)
-)];
+List<BoxShadow> boxShadow = [
+  BoxShadow(
+      color: Colors.black.withOpacity(0.2),
+      spreadRadius: 2,
+      blurRadius: 2,
+      offset: Offset(3, 3))
+];
